fc53b7d8a0313795d3229494acd637937ef98915
roudnas <dominikroudny@seznam.cz>
2021-01-14 20:37:57 +0100

    Added music queue

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv").config();
 const ytdl = require("ytdl-core");
 const { YTSearcher } = require("ytsearcher");
 const ytbsr = new YTSearcher(process.env.YTB_KEY);
-const fs = require("fs");
 const axios = require("axios");
 const prefix = "yo";
 
@@ -13,6 +12,10 @@ client.on("ready", () => {
 });
 
 let dispatcher = null;
+let audioQ = [];
+let voice = null;
+let isPlaying = false;
+let stream;
 
 client.on("message", async (msg) => {
   if (msg.author.bot) return;
@@ -25,12 +28,21 @@ client.on("message", async (msg) => {
 
     if (args[1] == "leave") {
       msg.member.voice.channel.leave();
+      if (dispatcher !== null) {
+        dispatcher.destroy();
+        dispatcher == null;
+        audioQ = [];
+        isPlaying = false;
+      }
       msg.channel.send("pici tak jo");
     }
 
     if (args[1] == "stop") {
       if (dispatcher !== null) {
+        audioQ = [];
         dispatcher.destroy();
+        dispatcher == null;
+        isPlaying = false;
       }
     }
 
@@ -47,6 +59,23 @@ client.on("message", async (msg) => {
     if (args[1] == "resume") {
       if (dispatcher !== null) {
         dispatcher.resume();
+      }
+    }
+
+    if (args[1] == ("q" || "queue")) {
+      if (audioQ.length > 0) {
+        const fieldsToAdd = [];
+        audioQ.forEach((el) => {
+          fieldsToAdd.push({ name: el.name, value: el.url });
+        });
+        const embedObject = {
+          color: 0x0099ff,
+          title: "Song queue",
+          fields: fieldsToAdd,
+        };
+        msg.channel.send({ embed: embedObject });
+      } else {
+        msg.channel.send(`Queue empty`);
       }
     }
 
@@ -73,26 +102,39 @@ client.on("message", async (msg) => {
     }
 
     if (args[1] == "play" && args[2] && msg.member.voice.channel) {
-      const query = msg.content.split("yo play")[1];
-      const streamOptions = { seek: 0, volume: 1 };
-
-      msg.member.voice.channel
-        .join()
-        .then((connection) => {
-          ytbsr.search(args[2]).then((res) => {
-            console.log(`new search: ${res.first.url}\n`);
-            const stream = ytdl(res.first.url, { filter: "audioonly" });
-            if (dispatcher !== null) dispatcher.destroy();
-            dispatcher = connection.play(stream, streamOptions);
-            console.log(`new dispatcher request: ${connection} \n`);
-            dispatcher.on("start", (start) => {
-              msg.channel.send(
-                `Playing dat shit boost it up yoooooooooooo \n${res.first.url}`
-              );
-            });
-          });
-        })
-        .catch((err) => console.error(err));
+      console.log("-------------");
+      if (voice === null) {
+        voice = await msg.member.voice.channel.join();
+      }
+      const search = await ytbsr.search(args[2]);
+      if (isPlaying) {
+        audioQ.push({ name: search.first.title, url: search.first.url });
+        msg.channel.send(`Mas to v queue mrdko \n${search.first.url}`);
+        console.log(`${search.first.url} added to queue`);
+      } else {
+        stream = ytdl(search.first.url, { filter: "audioonly" });
+        dispatcher = voice.play(stream);
+        isPlaying = true;
+      }
+      dispatcher.on("finish", async () => {
+        console.log("Dispatcher finished \n");
+        if (audioQ.length > 0) {
+          console.log("before q:" + [...audioQ]);
+          const { url } = audioQ.shift();
+          console.log(`Playing from queue, current q: ${[...audioQ]}`);
+          stream = ytdl(url, { filter: "audioonly" });
+          await voice.play(stream);
+        } else {
+          isPlaying = false;
+          dispatcher.destroy();
+          audioQ = [];
+          dispatcher = null;
+        }
+      });
+      dispatcher.on("start", (start) => {
+        console.log("Dispatcher started");
+        msg.channel.send(`Playing dat shit boost it up yoooooooooooo \n`);
+      });
     }
   }
 });
